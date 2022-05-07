ca5d1f5e4b5d3cb4e5b773989c4a1977745c75da
TokiLithuwix <Lithuwixxx@gmail.com>
2022-05-07 16:16:23 +0700

    to squere or not to squere (or root) *

--- 11-To-square(root)-or-not-to-square(root)/main.js
+++ 11-To-square(root)-or-not-to-square(root)/main.js
@@ -17,3 +17,11 @@ function squareOrSquareRoot(arr) {
 let result = squareOrSquareRoot(numsArray);
 console.log('init log');
 console.log(result);
+
+
+// function squareOrSquareRoot(array) {
+//     return array.map(x => {
+//         const r = Math.sqrt(x);
+//         return (r % 1 == 0) ? r : (x*x);
+//     });
+// }
